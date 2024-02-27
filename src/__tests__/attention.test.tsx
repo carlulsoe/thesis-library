9c82cac8e6b81c1933456fbe9d9b4aadaad9d7ab
Carl Ulsøe Christensen <201906950@post.au.dk>
2024-02-27 09:36:06 +0100

    test: adds a test

--- src/__tests__/attention.test.tsx
+++ src/__tests__/attention.test.tsx
@@ -1,6 +1,13 @@
 import { attention, setAttention } from '../extra/attention';
+import { Guid } from 'guid-typescript';
 
 test('can set attention', async () => {
   setAttention('this id');
   await expect(attention()).resolves.toBe('this id');
+});
+
+test('can set attetion to guid', async () => {
+  const uid = Guid.create().toString();
+  setAttention(uid);
+  await expect(attention()).resolves.toBe(uid);
 });
