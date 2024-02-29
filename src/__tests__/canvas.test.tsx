66df1c3a37eaf9fabe7d548281e7932077d5ecfa
Freja <frej0887@gmail.com>
2024-02-29 12:07:31 +0100

    test: updated canvas tests to have all 4 configuration of setup or not, and size or not

--- src/__tests__/canvas.test.tsx
+++ src/__tests__/canvas.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Canvas } from '../canvas';
 
-test('renders correctly', () => {
+test('renders correctly, setup, size', () => {
   const tree = renderer
     .create(
       <Canvas
@@ -13,5 +13,30 @@ test('renders correctly', () => {
       />
     )
     .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly, setup', () => {
+  const tree = renderer
+    .create(
+      <Canvas
+        setup={(ctx) => {
+          ctx.save();
+        }}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly, size', () => {
+  const tree = renderer
+    .create(<Canvas size={{ width: 200, height: 200 }} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly, no options', () => {
+  const tree = renderer.create(<Canvas />).toJSON();
   expect(tree).toMatchSnapshot();
 });
