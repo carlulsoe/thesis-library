9f74f4ba2b3869e752ca5b986655ea8337a3961a
Carl Ulsøe Christensen <201906950@post.au.dk>
2024-03-05 14:30:46 +0100

    fix: linter issues

--- src/extra/faceDetect.tsx
+++ src/extra/faceDetect.tsx
@@ -1,26 +1,11 @@
 import * as React from 'react';
 import { Platform } from 'react-native';
-//import { Camera } from 'expo-camera';
-//import * as VisionCamera from 'react-native-vision-camera';
-import {
-  Camera,
-  useCameraPermission,
-  useCameraDevice,
-} from 'react-native-vision-camera';
-import { Text, StyleSheet } from 'react-native';
+import { NativeFD } from './nativeFaceDetect';
 
 export function FD() {
   if (Platform.OS === 'web') {
     return <></>;
+  } else {
+    return NativeFD();
   }
-  const { hasPermission, requestPermission } = useCameraPermission();
-  if (hasPermission === false) {
-    requestPermission();
-  }
-  const device = useCameraDevice('back');
-
-  if (device == null) return <Text>No device</Text>;
-  return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
-  );
 }
