3bc8b03bb72e90e88e8fc928429ac4cb0765080e
Carl Ulsøe Christensen <201906950@post.au.dk>
2024-03-05 13:54:29 +0100

    feat: face detection that does not work on web

--- src/extra/faceDetect.tsx
+++ src/extra/faceDetect.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
 import { Platform } from 'react-native';
-import { Camera } from 'expo-camera';
-import * as FaceDetector from 'expo-face-detector';
+//import { Camera } from 'expo-camera';
+//import * as VisionCamera from 'react-native-vision-camera';
+import {
+  Camera,
+  useCameraPermission,
+  useCameraDevice,
+} from 'react-native-vision-camera';
+import { Text, StyleSheet } from 'react-native';
 
 export function FD() {
-  if (Platform.OS === 'android' || Platform.OS === 'ios') {
-    const handleFacesDetected = ({ faces }) => {
-      console.log(faces);
-    };
-
-    return (
-      <Camera
-        // other props
-        onFacesDetected={handleFacesDetected}
-        faceDetectorSettings={{
-          mode: FaceDetector.FaceDetectorMode.fast,
-          detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
-          runClassifications: FaceDetector.FaceDetectorClassifications.none,
-          minDetectionInterval: 100,
-          tracking: true,
-        }}
-      />
-    );
+  if (Platform.OS === 'web') {
+    return <></>;
   }
+  const { hasPermission, requestPermission } = useCameraPermission();
+  if (hasPermission === false) {
+    requestPermission();
+  }
+  const device = useCameraDevice('back');
+
+  if (device == null) return <Text>No device</Text>;
+  return (
+    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+  );
 }
