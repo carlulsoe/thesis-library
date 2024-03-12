a1e877e4636e84a0670d87a695cfaab4b20d5012
freja <frej0887@gmail.com>
2024-03-12 23:58:21 +0100

    feat: canvas colors + options menu

--- src/canvas/canvas.tsx
+++ src/canvas/canvas.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
-import {
-  View,
-  StyleSheet,
-  type GestureResponderEvent,
-  Button,
-} from 'react-native';
+import { View, StyleSheet, type GestureResponderEvent } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
+import {
+  MenuProvider,
+  Menu,
+  MenuOptions,
+  MenuTrigger,
+  MenuOption,
+} from 'react-native-popup-menu';
 
 export const Canvas = () => {
-  const [path, setPath] = useState('');
-  const addToPath = (s: string) => setPath(path + s);
+  const [currentColor, setColor] = useState(Colors.Black);
+  const [oldPaths, setOldPaths] = useState<IPath[]>([]);
+  const [currentPath, setCurrentPath] = useState('');
+  const addToPath = (s: string) => setCurrentPath(currentPath + s);
+  const addCurrentToOldPaths = () =>
+    setOldPaths([...oldPaths, { path: currentPath, color: currentColor }]);
 
   function handleStart(e: GestureResponderEvent) {
-    //console.log(e.nativeEvent);
     let x = e.nativeEvent.touches[0]?.locationX;
     let y = e.nativeEvent.touches[0]?.locationY;
     addToPath(`M${x} ${y} `);
@@ -23,22 +28,71 @@ export const Canvas = () => {
     addToPath(`L${x} ${y} `);
   }
 
+  function handleEnd() {
+    addCurrentToOldPaths();
+    setCurrentPath('');
+  }
+
+  function clearCanvas() {
+    setCurrentPath('');
+    setOldPaths([]);
+  }
+
+  // @ts-ignore
   return (
-    <View style={styles.outer}>
-      <View
-        style={styles.container}
-        onTouchStart={handleStart}
-        onTouchMove={handleMove}
-      >
-        <Svg width={1000} height={1000}>
-          <Path d={path} stroke={'red'} fillOpacity={0} />
-        </Svg>
+    <MenuProvider>
+      <View style={styles.outer}>
+        <View
+          style={styles.container}
+          onTouchStart={handleStart}
+          onTouchMove={handleMove}
+          onTouchEnd={handleEnd}
+        >
+          <Svg width={1000} height={1000}>
+            {oldPaths.map((path) => (
+              <Path
+                key={path.path}
+                d={path.path}
+                stroke={path.color}
+                fillOpacity={0}
+              />
+            ))}
+            <Path d={currentPath} stroke={currentColor} fillOpacity={0} />
+          </Svg>
+        </View>
+        <View>
+          <Menu>
+            <MenuTrigger text={'Options'} style={styles.button} />
+            <MenuOptions customStyles={optionsStyles}>
+              <MenuOption
+                onSelect={() => console.log(currentPath)}
+                text={'Print path to console'}
+              />
+              <MenuOption
+                onSelect={() => clearCanvas()}
+                text={'Clear canvas'}
+              />
+              <MenuOption
+                onSelect={() => setColor(Colors.Red)}
+                text={'Red stroke'}
+              />
+              <MenuOption
+                onSelect={() => setColor(Colors.Green)}
+                text={'Green stroke'}
+              />
+              <MenuOption
+                onSelect={() => setColor(Colors.Blue)}
+                text={'Blue stroke'}
+              />
+              <MenuOption
+                onSelect={() => setColor(Colors.Black)}
+                text={'Black stroke'}
+              />
+            </MenuOptions>
+          </Menu>
+        </View>
       </View>
-      <View>
-        <Button onPress={() => setPath('')} title={'Clear Canvas'} />
-        <Button onPress={() => console.log(path)} title={'Print Path'} />
-      </View>
-    </View>
+    </MenuProvider>
   );
 };
 
@@ -52,4 +106,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'stretch',
   },
+  button: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    textAlign: 'center',
+  },
 });
+
+const optionsStyles = {
+  optionsContainer: {
+    padding: 5,
+  },
+  optionWrapper: {
+    margin: 5,
+  },
+  optionTouchable: {
+    activeOpacity: 70,
+  },
+};
+
+enum Colors {
+  Red = '#FF0000',
+  Green = '#00FF00',
+  Blue = '#0000FF',
+  Black = '#000000',
+}
+
+interface IPath {
+  color: string;
+  path: string;
+}
