9b41a9be0d913951e0d72caed4b25ac5bde4a510
freja <frej0887@gmail.com>
2024-03-12 16:34:35 +0100

    test: deleted old tests

--- src/__tests__/canvas.test.tsx
+++ src/__tests__/canvas.test.tsx
@@ -2,40 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Canvas } from '../canvas';
 
-test('renders correctly, setup, size', () => {
-  const tree = renderer
-    .create(
-      <Canvas
-        setup={(ctx) => {
-          ctx.save();
-        }}
-        size={{ width: 200, height: 200 }}
-      />
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-test('renders correctly, setup', () => {
-  const tree = renderer
-    .create(
-      <Canvas
-        setup={(ctx) => {
-          ctx.save();
-        }}
-      />
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-test('renders correctly, size', () => {
-  const tree = renderer
-    .create(<Canvas size={{ width: 200, height: 200 }} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
 test('renders correctly, no options', () => {
   const tree = renderer.create(<Canvas />).toJSON();
   expect(tree).toMatchSnapshot();
