3ec05c97726af97518b44d709281560356744835
freja <frej0887@gmail.com>
2024-03-13 14:56:31 +0100

    test: deleted canvas test as it doesn't work

--- src/__tests__/canvas.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { Canvas } from '../canvas';
-
-test('renders correctly, no options', () => {
-  const tree = renderer.create(<Canvas />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
