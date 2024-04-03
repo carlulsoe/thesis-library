9b3457d14acfdbb4abb1941fab9560fa1687a560
Carl Ulsøe Christensen <201906950@post.au.dk>
2024-04-03 12:10:18 +0000

    feat: adds props to attention

--- src/attention/attention.tsx
+++ src/attention/attention.tsx
@@ -5,12 +5,26 @@ import { View } from 'react-native';
 import { Connect } from 'thesis-library';
 
 export function AddDetector() {
-  return Connector();
+  let dp = { receivingFunction: receive, sendingFunction: sending };
+  return Connector(dp);
+}
+
+interface DetectorProps {
+  receivingFunction: Function;
+  sendingFunction: Function;
+}
+
+function receive() {
+  console.log('received');
+}
+
+function sending() {
+  console.log('sending');
 }
 
 const ATTENTION_KEY = 'attention';
 
-const Connector = () => {
+const Connector = (dp: DetectorProps) => {
   const [fluidSharedObjects, setFluidSharedObjects] = useState(null);
   const initialObjects = { attention: SharedMap };
   const uuid = self.crypto.randomUUID();
@@ -20,9 +34,9 @@ const Connector = () => {
     if (fluidSharedObjects) {
       //@ts-ignore
       const { attention } = fluidSharedObjects.initialObjects;
-      Detector(uuid, attention, focus);
+      Detector(uuid, attention, focus, dp);
     }
-  }, [uuid, fluidSharedObjects]);
+  }, [uuid, fluidSharedObjects, dp]);
 
   return (
     //@ts-ignore
@@ -35,7 +49,12 @@ const Connector = () => {
   );
 };
 
-const Detector = (uuid: any, attention: SharedMap, focus: any) => {
+const Detector = (
+  uuid: any,
+  attention: SharedMap,
+  focus: any,
+  dp: DetectorProps
+) => {
   setInterval(() => IsFocused(focus, uuid, attention), 300);
   attention.addListener('valueChanged', (changed, local) => {
     if (changed.key !== ATTENTION_KEY) {
@@ -51,6 +70,7 @@ const Detector = (uuid: any, attention: SharedMap, focus: any) => {
       console.log(
         `CASE 1: This (${uuid}) is in focus from another (${changed.previousValue})`
       );
+      dp.receivingFunction();
       return;
     } else {
       const itIsAnotherDeviceToAnotherDevice = changed.previousValue !== uuid;
@@ -61,6 +81,7 @@ const Detector = (uuid: any, attention: SharedMap, focus: any) => {
       console.log(
         `CASE 2: value changed from this (${uuid}) to another (${attention.get(ATTENTION_KEY)})`
       );
+      dp.sendingFunction();
       return; //TODO change this to have functionallity
     }
   });
