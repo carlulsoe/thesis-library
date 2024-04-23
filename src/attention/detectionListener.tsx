f7869c9eacdb360e56d05cdf276dd8ffa306ef07
freja <frej0887@gmail.com>
2024-04-23 13:34:46 +0200

    refactor: nicer detectionListener.tsx

--- src/attention/detectionListener.tsx
+++ src/attention/detectionListener.tsx
@@ -2,31 +2,32 @@ import type { IValueChanged } from 'fluid-framework';
 import { type FocusProps, ATTENTION_KEY } from '../extra';
 import { type ConnectionContext } from '../extra';
 
-export function detectorListener(fp: FocusProps, context: ConnectionContext) {
+export function detectionListener(fp: FocusProps, context: ConnectionContext) {
+  if (!context || !fp.receivingFunction || !fp.sendingFunction) return;
+
   return (changed: IValueChanged, local: boolean) => {
-    if (changed.key !== ATTENTION_KEY) {
-      return;
-    }
+    if (changed.key !== ATTENTION_KEY) return;
     if (local) {
+      // CASE 1a: Nothing happened
       const itIsStillThisDevice = changed.previousValue === fp.uuid;
-      // CASE 1: value changed from another to this
-      if (itIsStillThisDevice) {
-        return;
-      }
-      if (!fp.receivingFunction || !context) return;
-      //fp.receivingFunction(context);
-      return;
+      if (itIsStillThisDevice) return;
+
+      // CASE 1b: value changed from another to this
+      fp.receivingFunction(context);
+      fp.multiUserSharing?.receivingFunction();
     } else {
+      // CASE 2a: Thing happened on other devices
       const itIsAnotherDeviceToAnotherDevice =
         changed.previousValue !== fp.uuid;
-      if (itIsAnotherDeviceToAnotherDevice) {
-        return;
+      if (itIsAnotherDeviceToAnotherDevice) return;
+
+      // CASE 2b: value changed from this to another
+      try {
+        fp.sendingFunction(context);
+        fp.multiUserSharing?.sendingFunction();
+      } catch (error) {
+        console.error(error);
       }
-      // CASE 2: value changed from this to another
-      if (!fp.sendingFunction || !context) return;
-      fp.sendingFunction(context);
-      fp.multiUserSharing?.sendingFunction();
-      return;
     }
   };
 }
