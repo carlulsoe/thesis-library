e01299478f0dcc453870181f95169406de470055
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2024-05-24 11:22:25 +0000

    feat: changed to Markdown

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { TextInput } from 'react-native';
+import { MarkdownTextInput } from '@expensify/react-native-live-markdown';
+import type { MarkdownStyle } from '@expensify/react-native-live-markdown';
 import {
   Canvas,
   type ConnectionContext,
@@ -26,15 +27,54 @@ export default function App() {
   return (
     <MultiDeviceAttention receivingFunction={receiver} sendingFunction={sender}>
       <Canvas variable={'canvas'} />
-      <TextInput
+      <MarkdownTextInput
         multiline
-        numberOfLines={4}
-        maxLength={40}
-        onChangeText={(t) => {
-          setText(t);
-        }}
+        onChangeText={setText}
         value={text}
+        markdownStyle={markdownStyle}
       />
     </MultiDeviceAttention>
   );
 }
+const FONT_FAMILY_MONOSPACE = 'monospace';
+
+const markdownStyle: MarkdownStyle = {
+  syntax: {
+    color: 'gray',
+  },
+  link: {
+    color: 'blue',
+  },
+  h1: {
+    fontSize: 25,
+  },
+  emoji: {
+    fontSize: 20,
+  },
+  blockquote: {
+    borderColor: 'gray',
+    borderWidth: 6,
+    marginLeft: 6,
+    paddingLeft: 6,
+  },
+  code: {
+    fontFamily: FONT_FAMILY_MONOSPACE,
+    fontSize: 20,
+    color: 'black',
+    backgroundColor: 'lightgray',
+  },
+  pre: {
+    fontFamily: FONT_FAMILY_MONOSPACE,
+    fontSize: 20,
+    color: 'black',
+    backgroundColor: 'lightgray',
+  },
+  mentionHere: {
+    color: 'green',
+    backgroundColor: 'lime',
+  },
+  mentionUser: {
+    color: 'blue',
+    backgroundColor: 'cyan',
+  },
+};
