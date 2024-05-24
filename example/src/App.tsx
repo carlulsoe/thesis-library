e3f5221a1478e790663220550bd13c1558d048f3
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2024-05-24 11:49:41 +0000

    feat: image add

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,12 +5,39 @@ import {
   Canvas,
   type ConnectionContext,
   MultiDeviceAttention,
+  ImageController,
 } from 'thesis-library';
+import type { S3ClientConfig } from '@aws-sdk/client-s3';
 
 export default function App() {
   // Run > `npx tinylicious` before normal start
   const [text, setText] = useState('');
+  const [selectedImage, setSelectedImage] = useState<string | undefined>();
   const LOC = 'text';
+
+  const ACCOUNT_ID = process.env.EXPO_PUBLIC_ACCOUNT_ID;
+  const ACCESS_KEY_ID = process.env.EXPO_PUBLIC_ACCESS_KEY_ID;
+  const SECRET_ACCESS_KEY = process.env.EXPO_PUBLIC_SECRET_ACCESS_KEY;
+
+  if (
+    ACCOUNT_ID === undefined ||
+    ACCESS_KEY_ID === undefined ||
+    SECRET_ACCESS_KEY === undefined
+  ) {
+    console.log('Could not load env variables.');
+    return;
+  }
+
+  const config: S3ClientConfig = {
+    region: 'auto',
+    endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: ACCESS_KEY_ID,
+      secretAccessKey: SECRET_ACCESS_KEY,
+    },
+  };
+
+  const { receive } = ImageController(selectedImage, setSelectedImage, config);
 
   const sender = (context: ConnectionContext) => {
     console.log('send', text);
@@ -19,8 +46,16 @@ export default function App() {
   };
 
   const receiver = (context: ConnectionContext) => {
+    receive(context);
     console.log('reci', text);
-    const newText = context.get(LOC);
+    let newText = context.get(LOC);
+    if (selectedImage) {
+      if (selectedImage.startsWith('data:')) {
+        let img = '![image](' + selectedImage + ')';
+        newText += '\n' + img;
+        setSelectedImage(undefined);
+      }
+    }
     setText(newText);
   };
 
